2bee9a72c5ee59ae263050617fc9d1edfe839c79
Vahid Mohammadi <vahmohh@gmail.com>
2020-12-01 10:40:47 +0100

    gfdgdfgdfgfgf

--- job.sh
+++ job.sh
@@ -3,8 +3,8 @@
 #SBATCH --tasks=80               # number of tasks     <---------- this is different to above
 #SBATCH --mem=40G                 # memory per node in MB (different units with suffix K|M|G|T)
 #SBATCH --time=2:00              # total runtime of job allocation ((format D-HH:MM:SS; first parts optional)
-#SBATCH --output=slurm.%j.out    # filename for STDOUT (%N: nodename, %j: job-ID)
-#SBATCH --error=slurm.%j.err     # filename for STDERR
+#SBATCH --output=finall.out    # filename for STDOUT (%N: nodename, %j: job-ID)
+#SBATCH --error=finall.err     # filename for STDERR
 
 # start here your MPI program
 python3 train.py
