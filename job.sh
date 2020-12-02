a92cd884de2f184892b57dfa7b7773cfad5b9880
vahmohh <vahmohh@gmail.com>
2020-12-02 22:20:23 +0100

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=gpu4          # partition (queue)
-#SBATCH --tasks=80               # number of tasks     <---------- this is different to above
+#SBATCH --tasks=1               # number of tasks     <---------- this is different to above
 #SBATCH --mem=40G                 # memory per node in MB (different units with suffix K|M|G|T)
 #SBATCH --time=2:00              # total runtime of job allocation ((format D-HH:MM:SS; first parts optional)
 #SBATCH --output=finall.out    # filename for STDOUT (%N: nodename, %j: job-ID)
