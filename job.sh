4332a45e3c4e40e9723e84398d42503a4bfea27f
vahmohh <vahmohh@gmail.com>
2020-12-03 00:13:45 +0100

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --partition=gpu          # partition (queue)
-#SBATCH --tasks=1               # number of tasks     <---------- this is different to above
+#SBATCH --partition=gpu4          # partition (queue)
+#SBATCH --tasks=10               # number of tasks     <---------- this is different to above
 #SBATCH --mem=40G                 # memory per node in MB (different units with suffix K|M|G|T)
 #SBATCH --output=finall.out    # filename for STDOUT (%N: nodename, %j: job-ID)
 #SBATCH --error=finall.err     # filename for STDERR
