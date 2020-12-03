91f50eaf3fe8fe23d1b82b999f98edb9ad457f4f
vahmohh <vahmohh@gmail.com>
2020-12-03 09:49:23 +0100

    Update job.sh

--- job.sh
+++ job.sh
@@ -4,7 +4,6 @@
 #SBATCH --mem=40G                 # memory per node in MB (different units with suffix K|M|G|T)
 #SBATCH --output=finall.out    # filename for STDOUT (%N: nodename, %j: job-ID)
 #SBATCH --error=finall.err     # filename for STDERR
-#SBATCH --time=2:00              # total runtime of job allocation ((format D-HH:MM:SS; first parts optional)
 
 # start here your MPI program
 python3 train.py
